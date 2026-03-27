c1ee9eb7bd76b79cbbd535ec3bdb489ae8c60bd4
rocha <foldzfofo@gmail.com>
2026-03-27 08:42:01 -0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,23 +4,20 @@ import threading
 target = input("Enter target IP: ")
 
 def scan(port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
 
-        if s.connect_ex((target, port)) == 0:
-            try:
-                banner = s.recv(1024).decode().strip()
-            except:
-                banner = "No banner"
+    if s.connect_ex((target, port)) == 0:
+        print(f"Port {port} is open")
 
-            print(f"Port {port} is open | Service: {banner}")
-
-        s.close()
-
-    except:
-        pass
+    s.close()
 
 threads = []
 
-for port in range(1, 1025
+for port in range(1, 1025):
+    t = threading.Thread(target=scan, args=(port,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
